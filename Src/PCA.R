9c9b44f30d0690aebc46e3e5e7153e0e0bbde754
Ayoub Vaziri-Moghadam <102460834+ayoub-vaziri@users.noreply.github.com>
2023-03-01 12:42:01 +0330

    Add files via upload

--- Src/PCA.R
+++ Src/PCA.R
@@ -1,7 +1,5 @@
 library(data.table)
-library(dplyr)
 library(factoextra)
-library(pheatmap)
 
 setwd("D:/Sharif University/Master/Lessons/5. Fifth Term/Thesis/")
 
@@ -10,31 +8,28 @@ setwd("D:/Sharif University/Master/Lessons/5. Fifth Term/Thesis/")
 train_data <- as.data.frame(fread("Res/batchCorrection/corrected_exprColorectal.csv"))
 bio_data <- fread("Res/exprBatchBioData/bioColorectal.txt", header = FALSE)$V1
 
-smps <- 1:52
-
 DEGsCRG <- fread("Res/PPI/DEGsCRGs.txt", header = FALSE)$V1
 
-valid_data <- train_data[smps,DEGsCRG]
-rownames(valid_data) <- valid_data$V1
-valid_data <- valid_data[,-1]
+expr_data <- train_data[,DEGsCRG]
+rownames(expr_data) <- expr_data$V1
+expr_data <- expr_data[,-1]
 
-bio_data <- bio_data[smps]
 bio_data <- as.factor(bio_data)
 levels(bio_data) <- c("Normal", "Tumor")
 bio_data <- as.character(bio_data)
 
-valid_data <- cbind(STATUS=bio_data, valid_data)
+expr_data <- cbind(STATUS=bio_data, expr_data)
 
-pca <- prcomp(valid_data[,-1], scale. = T)
+pca <- prcomp(expr_data[,-1], scale. = FALSE, center = TRUE)
 
 ppca <- fviz_pca_ind(pca,
                   label = "none",
                   palette = c("cyan3", "coral2"),
-                  habillage = valid_data$STATUS,
+                  habillage = expr_data$STATUS,
                   addEllipses = TRUE,
-                  ellipse.level=0.8)
+                  ellipse.level=0.75)
   
-png("Res/PCA/pcaplot.png", height = 1600, width = 2000, res = 300)
+png("Res/PCA/pcaplot.png", height = 2000, width = 2400, res = 300)
 ppca +
   theme_bw() +
   labs(title = "", x = "PC1", y = "PC2")
