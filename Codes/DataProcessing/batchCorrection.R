43a218dde5e27f4879a01b03092a0f86fd971117
Ayoub Vaziri-Moghadam <102460834+ayoub-vaziri@users.noreply.github.com>
2023-11-15 18:31:16 +0330

    Add files via upload

--- Codes/DataProcessing/batchCorrection.R
+++ Codes/DataProcessing/batchCorrection.R
@@ -5,7 +5,7 @@ library(sva)
 library(preprocessCore)
 
 # Set the current working directory to the project path
-setwd(PROJECT_PATH)
+setwd("PROJECT_PATH")
 
 #### Load data ####
 ###################
