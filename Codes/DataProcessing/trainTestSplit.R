51e75e8dc9a5f68af8035e2b9eff8d93aa678a1e
Ayoub Vaziri-Moghadam <102460834+ayoub-vaziri@users.noreply.github.com>
2023-12-02 12:07:04 +0330

    Add files via upload

--- Codes/DataProcessing/trainTestSplit.R
+++ Codes/DataProcessing/trainTestSplit.R
@@ -11,16 +11,16 @@ cancerSamples <- rownames(samples)[which(samples$group == 2)]
 normalSamples <- rownames(samples)[which(samples$group == 1)]
 
 trainingCancerSamples <- cancerSamples[sample(1:length(cancerSamples), ceiling(0.7*length(cancerSamples)))]
-testingCancerSamples <- cancerSamples[-which(cancerSamples %in% trainingCancerSamples)]
+validationCancerSamples <- cancerSamples[-which(cancerSamples %in% trainingCancerSamples)]
 
 trainingNormalSamples <- normalSamples[sample(1:length(normalSamples), ceiling(0.7*length(normalSamples)))]
-testingNormalSamples <- normalSamples[-which(normalSamples %in% trainingNormalSamples)]
+validationNormalSamples <- normalSamples[-which(normalSamples %in% trainingNormalSamples)]
 
 trainingSamples <- union(trainingCancerSamples, trainingNormalSamples)
-testingSamples <- union(testingCancerSamples, testingNormalSamples)
+validationSamples <- union(validationCancerSamples, validationNormalSamples)
 
 trainingSet <- samples[which(rownames(samples) %in% trainingSamples),]
-testingSet <- samples[which(rownames(samples) %in% testingSamples),]
+validationSet <- samples[which(rownames(samples) %in% validationSamples),]
 
 write.csv(trainingSet, "Results/DataProcessing/trainTestSplit/training_set.csv")
-write.csv(testingSet, "Results/DataProcessing/trainTestSplit/testing_set.csv")
+write.csv(validationSet, "Results/DataProcessing/trainTestSplit/validation_set.csv")
