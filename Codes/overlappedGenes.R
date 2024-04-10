2504fa1d38f6f80f35d6b60c4dbd068777532033
Ayoub Vaziri-Moghadam <102460834+ayoub-vaziri@users.noreply.github.com>
2024-04-10 10:07:22 +0330

    Add files via upload

---
+++ Codes/overlappedGenes.R
@@ -0,0 +1,31 @@
+library(ggvenn)
+library(data.table)
+
+# Set the current working directory to the project path
+setwd(project_path)
+
+degs <- fread("Res/differentialExpressionAnalysis/updown.txt", header = FALSE)$V1
+modules <- fread("Res/geneCoexpressionAnalysis/modulesGenes.txt", header = FALSE)$V1
+
+lstDegsModules <- list(
+  DEGs = degs,
+  M1 = modules
+)
+
+png("Res/overlappedGenes/DEGsModules.png", height = 1600, width = 1600, res = 300)
+ggvenn(data = lstDegsModules, 
+       show_elements = FALSE, 
+       show_percentage = FALSE,
+       label_sep = "\n", 
+       fill_color = c("blue", "red"),
+       fill_alpha = 0.3,
+       text_size = 6,
+       set_name_size = 5.5,
+       text_color = "black",
+       stroke_alpha = 0.5,
+       stroke_size = 0.2,
+)
+dev.off()
+
+DEGsModules <- intersect(degs, modules)
+write.table(DEGsModules, file = "Res/overlappedGenes/DEGsModules.txt", quote = FALSE, row.names = FALSE, col.names = FALSE)
